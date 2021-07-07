25466ee6c018cf864461c03de230e2feac58d0b3
kokhoutang <84445160+kokhoutang@users.noreply.github.com>
2021-07-07 21:35:51 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+          val btnDone: Button = findViewById(R.id.done_button)
+        
+        btnDone.setOnClickListener(){
+
+            val tvInput: TextView = findViewById(R.id.nickname_edit)
+            val tvResult: TextView = findViewById(R.id.nickname_text)
+            tvResult.text = tvInput.text
+            tvResult.visibility = View.VISIBLE
+            btnDone.visibility = View.GONE
+            tvInput.visibility = View.GONE
+        }
     }
 }
